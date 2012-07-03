c588781e52594e4cc159aa98b5c6def900160a00
zhongjj <zhongjj@lemote.com>
2012-07-03 20:12:05 +0800

    init the settings.py add account urls to urls.py

--- sw/settings.py
+++ sw/settings.py
@@ -4,6 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
+    ('zhongjj','zhongjj@lemote.com'),
     # ('Your Name', 'your_email@example.com'),
 )
 
@@ -11,8 +12,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/home/mole/sw/database/jj.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -106,6 +107,7 @@ ROOT_URLCONF = 'sw.urls'
 WSGI_APPLICATION = 'sw.wsgi.application'
 
 TEMPLATE_DIRS = (
+    '/home/mole/sw/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -119,7 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'account', 
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
@@ -152,3 +155,10 @@ LOGGING = {
         },
     }
 }
+
+#mail setting 
+EMAIL_HOST = '192.168.2.8'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'zhongjj@lemote.com'
+EMAIL_HOST_PASSWORD = 'Zhjj123-'
+
--- sw/urls.py
+++ sw/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+def i18n_javascript(request):
+    return admin.site.i18n_javascript(request)
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sw.views.home', name='home'),
-    # url(r'^sw/', include('sw.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/jsi18n', i18n_javascript),
+    url(r'admin/', include(admin.site.urls)),
+    url(r'register/$','account.views.register'),
+    url(r'password_change/$','account.views.password_change'),
+    url(r'logout/$','account.views.logout'),
+    url(r'login/$','account.views.login'),
+    url(r'^$','account.views.login'),
 )
