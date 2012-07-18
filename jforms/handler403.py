016617ef524616c2da6bc6df77509781e114c0d7
zhongjj <zhongjj@lemote.com>
2012-07-18 21:32:30 +0800

    handler403

--- jforms/handler403.py
+++ jforms/handler403.py
@@ -5,7 +5,6 @@ def handler403(request):
     content = {}
     if request.user.is_authenticated():
         content.update({"username":request.user.first_name})
-
-   return render_to_response("jforms/message.html",{"message":"没有权限执行该操作",}) 
+    return render_to_response("jforms/message.html",{"message":"没有权限执行该操作",}) 
 
 
