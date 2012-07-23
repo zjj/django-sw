48ee93b8686ca3fa032fbc958576232644df8ea0
zhongjj <zhongjj@lemote.com>
2012-07-23 10:07:23 +0800

    添加"更多"需求历史

--- jforms/views.py
+++ jforms/views.py
@@ -19,6 +19,8 @@ def myhome(request):
     adduser(content,request.user)
     reqs = Requirement.objects.filter(author=request.user)
     reqs = reqs.order_by("-time")
+    if len(reqs) > 10:
+        content.update({"more_reqs":True}) 
     req_set = set()
     for i in reqs:
         req_set.add(i.index)
@@ -188,9 +190,51 @@ def myhome(request):
      
     content.update({"list2":ret})  
 
-    
     return render_to_response("jforms/myhome.html", content)
 
+
+def myreqs(request):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect('/process/')
+    content = {}
+    adduser(content,request.user)
+    reqs = Requirement.objects.filter(author=request.user)
+    reqs = reqs.order_by("-time")
+    req_set = set()
+    for i in reqs:
+        req_set.add(i.index)
+
+    reqs = []
+    ret = []
+    for i in req_set:
+        req = Requirement.objects.filter(index=i)
+        req = req[len(req)-1]
+        reqs.append(req)
+    for i in reqs:
+        each = {"requirement":i}
+        try:
+            rh = History.objects.filter(requirement=i,stage="requirement")
+            rh = rh[len(rh)-1]
+            each.update({"req":rh})
+        except:
+            pass
+        try: 
+            ph = History.objects.filter(requirement=i,stage="predev")
+            ph = ph[len(ph)-1]
+            each.update({"predev":ph})
+        except:
+            pass
+        try:
+            dh = History.objects.filter(requirement=i,stage="dev")
+            dh = dh[len(dh)-1]
+            each.update({"dev":dh})
+        except:
+            pass
+        ret.append(each)
+     
+    content.update({"list":ret})  
+    
+    return render_to_response("jforms/myreqs.html", content)
 
 
 
