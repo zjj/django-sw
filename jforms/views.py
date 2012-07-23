10fd100407ade623834fdb2d4814d68f49946750
zhongjj <zhongjj@lemote.com>
2012-07-23 09:53:11 +0800

    添加主页中会签历史相关

--- jforms/views.py
+++ jforms/views.py
@@ -127,6 +127,67 @@ def myhome(request):
         ret.append(each)
      
     content.update({"list1":ret})  
+
+    #singed items
+    ret = []
+    user = request.user
+
+    rc = RequirementConfirm.objects.filter(signature=user,signed=True)
+    for i in rc:
+        ret.append(i.requirement)
+
+    rjc = RequireJudgementConfirm.objects.filter(user=user,signed=True)
+    for i in rjc:
+        ret.append(i.requirement)
+
+    pdjc = PreDevJudgementConfirm.objects.filter(user=user,signed=True)
+    for i in pdjc:
+        ret.append(i.predevjudge.predev.requirement)
+
+    djc = DevJudgementConfirm.objects.filter(user=user,signed=True)
+    for i in djc:
+        ret.append(i.devjudge.dev.requirement)
+
+    tjc = TestJudgementConfirm.objects.filter(signature=user,signed=True)
+    for i in tjc:
+        ret.append(i.testjudge.devjudge.dev.requirement)
+
+    reqs = ret 
+    req_set = set()
+    for i in reqs:
+        req_set.add(i.index)
+        if len(req_set) >= 10:
+            break
+    reqs = []
+    ret = []
+    for i in req_set:
+        req = Requirement.objects.filter(index=i)
+        req = req[len(req)-1]
+        reqs.append(req)
+    for i in reqs:
+        each = {"requirement":i}
+        try:
+            rh = History.objects.filter(requirement=i,stage="requirement")
+            rh = rh[len(rh)-1]
+            each.update({"req":rh})
+        except:
+            pass
+        try: 
+            ph = History.objects.filter(requirement=i,stage="predev")
+            ph = ph[len(ph)-1]
+            each.update({"predev":ph})
+        except:
+            pass
+        try:
+            dh = History.objects.filter(requirement=i,stage="dev")
+            dh = dh[len(dh)-1]
+            each.update({"dev":dh})
+        except:
+            pass
+        ret.append(each)
+     
+    content.update({"list2":ret})  
+
     
     return render_to_response("jforms/myhome.html", content)
 
